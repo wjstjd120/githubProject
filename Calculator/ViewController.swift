43bb764afa2104b63da15d5c17c9eda823a7b8d6
wjstjd120 <wjstjd120@naver.com>
2024-06-03 16:01:08 +0900

    head 지움

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,14 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         //아아아아아
         print("Hello KDH")
-<<<<<<< HEAD
-<<<<<<< HEAD
-        // Do any additional setup after loading the view.
-=======
->>>>>>> 878c30479613af0e6f9f14a9cdbb69a5d563749b
-=======
-        // Do any additional setup after loading the view.
->>>>>>> 1cd8881a80075ac3b7d6ad10a242db6e875b874c
     }
 
 
